67f2be715291f7ac16aad48da61b80582a891cc8
OleksandrVaniuta <vaniutasa@gmail.com>
2023-05-11 15:26:12 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 // import { lazy } from 'react';
 import { Layout } from './Layout/Layout';
 import { ContactMain } from './ContactsMain/ContactsMain';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 
 export default function App() {
   return (
